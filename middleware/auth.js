def878499f3c32f606bf98990353c6e765c02bfd
VijayOdedara899 <143780409+VijayOdedara899@users.noreply.github.com>
2025-12-19 10:15:24 +0530

    auth baypass for renderar

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,22 +3,26 @@ const User = require('../models/user');
 const { error } = require('../utils/response');
 
 module.exports = async function(req, res, next) {
-  const authHeader = req.headers['authorization'] || req.headers['Authorization'];
-  if (!authHeader) return error(res, 'No authorization header', 401);
+  // const authHeader = req.headers['authorization'] || req.headers['Authorization'];
+  // if (!authHeader) return error(res, 'No authorization header', 401);
 
-  const parts = authHeader.split(' ');
-  if (parts.length !== 2 || parts[0] !== 'Bearer') return error(res, 'Invalid authorization header', 401);
+  // const parts = authHeader.split(' ');
+  // if (parts.length !== 2 || parts[0] !== 'Bearer') return error(res, 'Invalid authorization header', 401);
 
-  const token = parts[1];
+  // const token = parts[1];
 
-  try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    // attach user id to req
-    req.user = { id: payload.id };
+  // try {
+  //   const payload = jwt.verify(token, process.env.JWT_SECRET);
+  //   // attach user id to req
+  //   req.user = { id: payload.id };
+  //   // optionally fetch user from DB
+  //   req.currentUser = await User.findById(payload.id).select('-password');
+  //   next();
+  // } catch (err) {
+  //   return error(res, 'Invalid or expired token', 401);
+  // }
+  req.user = { id: "6916ad6f66229a11b6d7b720" };
     // optionally fetch user from DB
-    req.currentUser = await User.findById(payload.id).select('-password');
+    req.currentUser = await User.findById("6916ad6f66229a11b6d7b720").select('-password');
     next();
-  } catch (err) {
-    return error(res, 'Invalid or expired token', 401);
-  }
 };
--- server.js
+++ server.js
@@ -19,31 +19,18 @@ const PORT = process.env.PORT || 3000;
 connectDB(process.env.MONGO_URI || 'mongodb://localhost:27017/unitydb');
 
 // Middlewares
-app.set('trust proxy', 1); 
-
 app.use(helmet());
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(assetbundlesDirectory));
 
-// const limiter = rateLimit({
-//   windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS || '60000'),
-//   max: parseInt(process.env.RATE_LIMIT_MAX || '200')
-// });
-// app.use(limiter);
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  validate: { xForwardedForHeader: false },
+  windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS || '60000'),
+  max: parseInt(process.env.RATE_LIMIT_MAX || '200')
 });
 app.use(limiter);
-app.use(cors({
-  origin: 'https://bodega-backend-vtp8.onrender.com',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
 
 // Routes
 app.use('/auth', authRoutes);
